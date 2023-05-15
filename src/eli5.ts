86af281907255d5fc52882d54bd3ffa127e0e485
Alexander Cornett <ge42nir@mytum.de>
2023-05-15 12:24:23 +0200

    new structure (string outside of function

--- src/eli5.ts
+++ src/eli5.ts
@@ -1,3 +1,7 @@
+
+/*
+The Interface for the json when sending the request
+*/
 interface RequestBody {
   model: string
   messages: {
@@ -5,6 +9,10 @@ interface RequestBody {
     content: string
   }[]
 }
+
+/*
+The Interface to define the response expected when requesting from openai
+*/
 interface Response {
   id: string
   object: string
@@ -21,20 +29,32 @@ interface Response {
     index: number
   }[]
 }
+/*
+The request send to openai in the form of a string, so that It can later be modified. 
+*/
 const request_body = `{"model":"gpt-3.5-turbo","messages":[{"role":"system","content":"You are a Computer Science Teacher talking to his students and the students do not have a deep technical understanding. So the teacher tries to stay simple"},{"role":"user","content":""}]}`
+
+/*
+The question that will later be send to openai inorder to get an answer by chatgtp
+*/
+const question =
+    'The following text describes a programming project that is currently in development. Explain to me what the project is trying to achieve without telling me how they are doing so. Please use around 50 words and do not get too technical'
 
 void openAIRequestTurbo()
 
+
+/*
+THis methond send a request to openai and displays and console log the answer. The details for the question later have to be passed on as a parameter
+*/
+
 async function openAIRequestTurbo() {
-  const question =
-    'The following text describes a programming project that is currently in development. Explain to me what the project is trying to achieve without telling me how they are doing so. Please use around 50 words and do not get too technical'
+  
+  const jsonRequestBody = JSON.parse(request_body) as RequestBody;
+  jsonRequestBody.messages[1].content = question;
 
-  const jsonRequestBody = JSON.parse(request_body) as RequestBody
-
-  // jsonRequestBody.messages[1].content = question;
-  let response: Response = new Response()
+  
   try {
-    response = await fetch('https://api.openai.com/v1/chat/completions', {
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
       headers: {
         'Content-Type': 'application/json',
         Authorization: 'Bearer ' + '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' // process.env.OPENAI_API_KEY
