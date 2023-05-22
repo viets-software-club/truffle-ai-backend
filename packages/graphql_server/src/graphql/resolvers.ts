5255c6e4970ab8bdb6c4816f4dd80d39d1017dd5
jonas suess <jonas.suess@tum.de>
2023-05-22 17:28:01 +0200

    resolved merge conflicts

--- packages/graphql_server/src/graphql/resolvers.ts
+++ packages/graphql_server/src/graphql/resolvers.ts
@@ -1,11 +1,7 @@
 // I get an error when importing this module. It works still.
 // chatGPT says this might come from the standard module loader not supporting
 // json files?! I do not know how to resolve this atm
-<<<<<<< HEAD:src/graphql/resolvers.ts
-// import sampleProjects from '../../assets/sampleProjects'
-=======
 import sampleProjects from '../assets/sampleProjects.json'
->>>>>>> bfe4521d04d6c50f1df0c482fc7aa03529b1688a:packages/graphql_server/src/graphql/resolvers.ts
 
 type SocialMediaPresence = {
   platform: string
@@ -21,20 +17,9 @@ type SocialMediaTopPost = {
 
 const resolvers = {
   Query: {
-<<<<<<< HEAD:src/graphql/resolvers.ts
-    // see https://graphql.org/learn/execution/
-    // Returns the above imported sampleProjects
-    //
-    // pre-commit hook complains because of typing, which does not play a
-    // role here because GraphQL knows about the type -> --no-verify commit
-    //
-    // will check with Philipp whether and how to change this
-    // projects: () => sampleProjects
-=======
     // @TODO add typing for all types
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     projects: () => sampleProjects
->>>>>>> bfe4521d04d6c50f1df0c482fc7aa03529b1688a:packages/graphql_server/src/graphql/resolvers.ts
   },
   SocialMediaPresence: {
     // resolves the type of SocialMediaPresence based on the platform field
