9516e8ff7065eccb15db31ff7ff1c1d84378bd83
jonas suess <jonas.suess@tum.de>
2023-05-22 18:48:30 +0200

    BACK-42 added testing method into new index file

--- packages/graphql_server/src/index.ts
+++ packages/graphql_server/src/index.ts
@@ -1,3 +1,6 @@
 import app from './server'
+import { getCompanyInfosFromLinkedIn } from './LinkedInScrapingService'
 
+
+void getCompanyInfosFromLinkedIn('google').then((r) => console.log(r))
 void app.listen({ host: '0.0.0.0', port: process.env.SERVER_PORT || 3001 })
--- src/listen.ts
+++
@@ -1,5 +0,0 @@
-import app from './server'
-import { getCompanyInfosFromLinkedIn } from './LinkedInScrapingService'
-
-void getCompanyInfosFromLinkedIn('ferrari').then((r) => console.log(r))
-void app.listen({ port: 3000 })
