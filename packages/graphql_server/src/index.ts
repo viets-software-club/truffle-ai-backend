d4c3631859c85a2d4dbe6a081ac27503e657d514
jonas suess <jonas.suess@tum.de>
2023-05-22 18:54:44 +0200

    BACK-42 run prettier

--- packages/graphql_server/src/index.ts
+++ packages/graphql_server/src/index.ts
@@ -1,6 +1,5 @@
 import app from './server'
 import { getCompanyInfosFromLinkedIn } from './LinkedInScrapingService'
 
-
 void getCompanyInfosFromLinkedIn('google').then((r) => console.log(r))
 void app.listen({ host: '0.0.0.0', port: process.env.SERVER_PORT || 3001 })
