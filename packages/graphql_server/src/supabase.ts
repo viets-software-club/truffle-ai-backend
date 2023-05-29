58a12bc93715ed64eef0e76e344c4d8a2e00113c
Tom Woehrle <thomas.e.woehrle@gmail.com>
2023-05-29 14:32:55 +0200

    Address Clemens' and Philipp's review comments

--- packages/graphql_server/src/supabase.ts
+++ packages/graphql_server/src/supabase.ts
@@ -2,7 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '../types/supabase'
 
 const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY)
-
-// const supabase = createClient<Database>('url', 'key')
-
 export default supabase
