7550b652dca271f6ad92ffab6611fa5570e82d73
acreturs <acornett@gmx.de>
2023-06-08 09:30:44 +0200

    tests improved

--- packages/graphql_server/src/test/githubApi.test.ts
+++ packages/graphql_server/src/test/githubApi.test.ts
@@ -3,5 +3,5 @@ import { getRepositoryTopics } from '../api/githubApi'
 async function testGetRepositoryTopics(repoFounder: string, repoName: string) {
   //get the real comments from method in hackerNewsScraper. E.g. searchHackerNewsStories('SuperAGI')
 
-  console.log(await getRepositoryTopics(repoFounder, repoName))
+  console.log(await getRepositoryTopics(repoFounder, repoName, 'token'))
 }
