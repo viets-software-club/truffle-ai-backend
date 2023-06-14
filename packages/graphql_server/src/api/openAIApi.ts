6afe0da8c65cd0a215d956ba4862cc0275060f69
acreturs <acornett@gmx.de>
2023-06-14 11:09:17 +0200

    currently in development. Please provide further information on how we should use enums in this project

--- packages/graphql_server/src/api/openAIApi.ts
+++ packages/graphql_server/src/api/openAIApi.ts
@@ -88,7 +88,148 @@ async function getHackernewsSentiment(comments: string) {
     }
   } catch (error) {
     console.log('OpenAI request did not work out: ', error)
-    return null
+    return null // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async function categorizeProjectSpecific(readMeOrCategory: string, categoryGeneral: number) {
+      //The following 3 variables are the lists of categories used to define the project
+      const listOfCategoriesDeveloperTools: string[] = [
+        'Code Editors',
+        'Version Control',
+        'Continuous Integration',
+        'Testing Frameworks',
+        'Package Managers',
+        'Integrated Development Environments',
+        'Debuggers',
+        'Profilers',
+        'Build Tools',
+        'Code Quality'
+      ]
+
+      const listOfCategoriesInfrastructure: string[] = [
+        'Cloud Computing',
+        'Virtualization',
+        'Containerization',
+        'Orchestration',
+        'Monitoring',
+        'Networking',
+        'Databases',
+        'Load Balancing',
+        'Content Delivery Networks',
+        'Identity Management'
+      ]
+
+      const listOfCategoriesMLAI: string[] = [
+        'Machine Learning',
+        'Deep Learning',
+        'Natural Language Processing',
+        'Computer Vision',
+        'Cybersecurity'
+      ]
+
+      const questionCategoriesSpecific = //this is the question send to openAI
+        'These categories should be used to categorize a software engineering project. Please choose two of the categories from the list that describe this project the best based on these words or readme (Your response should only consist of the two words you choose, separated by a comma): '
+
+      let categoriesSpecific: string[] //checks which general category it is
+      switch (categoryGeneral) {
+        case 1:
+          categoriesSpecific = listOfCategoriesDeveloperTools
+          break
+        case 2:
+          categoriesSpecific = listOfCategoriesInfrastructure
+          break
+        case 3:
+          categoriesSpecific = listOfCategoriesMLAI
+          break
+        case 4: //if chatgtp says the categories are not specific enough we need t use the readme file to put it into two of the categories
+          categoriesSpecific = listOfCategoriesDeveloperTools.concat(listOfCategoriesInfrastructure)
+          categoriesSpecific = categoriesSpecific.concat(listOfCategoriesMLAI)
+          break
+        default:
+          console.log('Invalid categoryGeneral value')
+          return null
+      }
+
+      const requestBodyCategories: RequestBodyOpenAI = {
+        model: model,
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are a professor trying to categorize a project. You have to read something about the project and then give it two of the categories according to your evaluation. Even if you do not think that you can evaluate it. Just do it to the best of your abilities'
+          },
+          {
+            role: 'user',
+            content: categoriesSpecific.join(' ,') + questionCategoriesSpecific + readMeOrCategory
+          }
+        ]
+      }
+
+      try {
+        const response = await axios.post(openAIapiUrl, requestBodyCategories, { headers })
+        const data = response.data as ResponseBodyOpenAi
+
+        if (!data?.choices[0]?.message?.content) {
+          console.log(errorMessage)
+          return null
+        } else {
+          const content: string = data.choices[0].message.content
+          return content
+        }
+      } catch (error) {
+        console.log('AI request did not work: ', error)
+        return null
+      }
+    }
+
+    /**
+     * Categorizes a software engineering project into a general category based on its README or provided category.
+     * @param readMeOrCategory - The project's README or category.
+     * @returns The two best-fitting specific categories for the project, based on the general category.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async function categorizeProjectGeneral(categories: string, readme: string) {
+      const questionCategoriesGeneral =
+        'These 3 categroies should be used to categorize a software engineering project. 1 for developer tools, 2 for Infrastructure, 3 for Machine Learning and Artifical Inteligence. Apart from the number do not respond with anything): '
+
+      const request_body_Categories = {
+        model: model,
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are a professor trying to categorize a project. You have to read something about the project and then give it two of the categories according to your evaluation. Even if you do not think that you can evaluate it. Just do it to the best of your abilities'
+          },
+          { role: 'user', content: '' }
+        ]
+      }
+
+      const listOfCategories: string[] = [
+        'Developer Tools',
+        'Infrastrcuture',
+        'Machine Learning and Aritfical Inteligence'
+      ]
+      request_body_Categories.messages[1].content =
+        listOfCategories.join(' ,') + questionCategoriesGeneral + categories
+      try {
+        const response = await axios.post(openAIapiUrl, request_body_Categories, { headers })
+        //returns a number that decides which general categorie we are using
+        const data = response.data as ResponseBodyOpenAi
+        if (!data?.choices[0]?.message?.content) {
+          console.log(errorMessage)
+          return null
+        } else {
+          const content: string = data?.choices[0]?.message?.content
+          const num = parseInt(content)
+          if (num !== 1 && num !== 2 && num !== 3) {
+            return categorizeProjectSpecific(readme, 4)
+          } else {
+            return categorizeProjectSpecific(categories, num)
+          }
+        }
+      } catch (error) {
+        console.log('AI request did not work: ', error)
+        return null
+      }
+    }
   }
 }
 
@@ -99,50 +240,56 @@ async function getHackernewsSentiment(comments: string) {
  * @param categoryGeneral - The general category of the project (1 for developer tools, 2 for infrastructure, 3 for ML/AI).
  * @returns The two best-fitting specific categories for the project.
  */
+
+enum DeveloperToolCategory {
+  CodeEditors = 'Code Editors',
+  VersionControl = 'Version Control',
+  ContinuousIntegration = 'Continuous Integration',
+  TestingFrameworks = 'Testing Frameworks',
+  PackageManagers = 'Package Managers',
+  IDEs = 'Integrated Development Environments',
+  Debuggers = 'Debuggers',
+  Profilers = 'Profilers',
+  BuildTools = 'Build Tools',
+  CodeQuality = 'Code Quality'
+}
+
+enum InfrastructureCategory {
+  CloudComputing = 'Cloud Computing',
+  Virtualization = 'Virtualization',
+  Containerization = 'Containerization',
+  Orchestration = 'Orchestration',
+  Monitoring = 'Monitoring',
+  Networking = 'Networking',
+  Databases = 'Databases',
+  LoadBalancing = 'Load Balancing',
+  ContentDeliveryNetworks = 'Content Delivery Networks',
+  IdentityManagement = 'Identity Management'
+}
+
+enum MLAICategory {
+  MachineLearning = 'Machine Learning',
+  DeepLearning = 'Deep Learning',
+  NaturalLanguageProcessing = 'Natural Language Processing',
+  ComputerVision = 'Computer Vision',
+  Cybersecurity = 'Cybersecurity'
+}
+
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function categorizeProjectSpecific(readMeOrCategory: string, categoryGeneral: number) {
   //The following 3 variables are the lists of categories used to define the project
-  const listOfCategoriesDeveloperTools: string[] = [
-    'Code Editors',
-    'Version Control',
-    'Continuous Integration',
-    'Testing Frameworks',
-    'Package Managers',
-    'Integrated Development Environments',
-    'Debuggers',
-    'Profilers',
-    'Build Tools',
-    'Code Quality'
-  ]
-
-  const listOfCategoriesInfrastructure: string[] = [
-    'Cloud Computing',
-    'Virtualization',
-    'Containerization',
-    'Orchestration',
-    'Monitoring',
-    'Networking',
-    'Databases',
-    'Load Balancing',
-    'Content Delivery Networks',
-    'Identity Management'
-  ]
-
-  const listOfCategoriesMLAI: string[] = [
-    'Machine Learning',
-    'Deep Learning',
-    'Natural Language Processing',
-    'Computer Vision',
-    'Cybersecurity'
-  ]
 
   const questionCategoriesSpecific = //this is the question send to openAI
     'These categories should be used to categorize a software engineering project. Please choose two of the categories from the list that describe this project the best based on these words or readme (Your response should only consist of the two words you choose, separated by a comma): '
 
-  let categoriesSpecific: string[] //checks which general category it is
+  let categoriesSpecific: SpecificCategory[] //checks which general category it is
   switch (categoryGeneral) {
     case 1:
-      categoriesSpecific = listOfCategoriesDeveloperTools
+      categoriesSpecific = [SpecificCategory.CodeEditors,
+                             SpecificCategory.VersionControl,
+                             SpecificCategory.ContinuousIntegration
+
       break
     case 2:
       categoriesSpecific = listOfCategoriesInfrastructure
