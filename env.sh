d18f595046ec4ecaa02e33014eadd3a2a4a9a9ef
Philipp Czernitzki <i@pczern.dev>
2023-06-18 13:30:46 +0200

    update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# exports environment variables based on NODE_ENV
 if [ "$NODE_ENV" != "production" ] && [ "$NODE_ENV" != "test" ] && [ "$NODE_ENV" != "build" ]
 then
+echo '[env.sh] Using .env.dev environment variables'
 export $(cat ../../.env.dev | grep -v \"#\" | xargs)
 else
-echo 'No development environment loaded'
+echo '[env.sh] Did not load environment variables'
 fi
