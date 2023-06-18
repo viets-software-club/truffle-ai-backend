53d8ef91aebe0208080e70689006ce53e2e50bde
Tom Woehrle <thomas.e.woehrle@gmail.com>
2023-06-18 19:57:40 +0200

    Adjust imports for updateProject, supabaseUtils and utils.ts

--- packages/api/types/supabaseUtils.d.ts
+++ packages/api/types/supabaseUtils.d.ts
@@ -1,17 +1,17 @@
 import { Database } from './supabase'
 
-type ProjectInsertion = Database['public']['Tables']['project']['Insert']
-type ProjectUpdate = Database['public']['Tables']['project']['Update']
+export type ProjectInsertion = Database['public']['Tables']['project']['Insert']
+export type ProjectUpdate = Database['public']['Tables']['project']['Update']
 
-type OrganizationInsertion = Database['public']['Tables']['organization']['Insert']
-type OrganizationUpdate = Database['public']['Tables']['organization']['Update']
+export type OrganizationInsertion = Database['public']['Tables']['organization']['Insert']
+export type OrganizationUpdate = Database['public']['Tables']['organization']['Update']
 
-type PersonInsertion = Database['public']['Tables']['associated_person']['Insert']
+export type PersonInsertion = Database['public']['Tables']['associated_person']['Insert']
 
-type FoundedByInsertion = Database['public']['Tables']['founded_by']['Insert']
+export type FoundedByInsertion = Database['public']['Tables']['founded_by']['Insert']
 
 // has to be named ProjectInfo because otherwise there could be name conflict with supabaseType
-type ProjectInfo = {
+export type ProjectInfo = {
   name: string
   owner: string
 }
