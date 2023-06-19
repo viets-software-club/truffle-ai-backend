e5abd61a09a6f496da93331ebc7fb8216215a3a2
Maximilian von Boxberg <70841946+2015builder@users.noreply.github.com>
2023-06-19 13:37:55 +0200

    Delete changes in types.ts of starHistory

--- packages/graphql_server/src/starHistory/types.ts
+++ packages/graphql_server/src/starHistory/types.ts
@@ -11,20 +11,3 @@ export type StarRecord = {
   date: string
   count: number
 }
-
-export type ForkRecord = StarRecord
-
-export type ForksData = {
-  length: number
-  headers: {
-    link?: string
-  }
-  status: number
-  created_at: string
-}
-
-export type IssueRecord = StarRecord
-
-export type IssueData = ForksData
-
-export type TimeFrame = 'day' | 'week' | 'month' | '3 month' | '6 month' | 'year'
