d18c2747d3b69de7c565942c3e27d8a1d6cd5b30
Philipp Czernitzki <i@pczern.dev>
2023-06-21 15:43:27 +0200

    update graphql server with api package

--- packages/graphql_server/types/global.d.ts
+++ packages/graphql_server/types/global.d.ts
@@ -6,6 +6,10 @@ declare global {
       NODE_ENV: 'development' | 'production'
       SUPABASE_URL: string
       SUPABASE_API_KEY: string
+      SCRAPING_BOT_API_KEY: string
+      SCRAPING_BOT_USER_NAME: string
+      OPENAI_API_KEY: string
+      GITHUB_API_TOKEN: string
     }
   }
 }
