1a34284aeaea835f51d92fedbb5c8a56526ff88e
acreturs <acornett@gmx.de>
2023-06-22 17:46:43 +0200

    modified tests

--- packages/graphql_server/src/test/githubApi.test.ts
+++ packages/graphql_server/src/test/githubApi.test.ts
@@ -61,59 +61,171 @@ export async function testGetRepositoryTopics(githubToken: string) {
 }
 
 //test function that calls the method and prints out all contributors
+// eslint-disable-next-line @typescript-eslint/require-await
+async function calculatePercentageDifference(expected: number, actual: number) {
+  const diff = Math.abs(actual - expected)
+  const percentageDiff = (diff / expected) * 100
+  return percentageDiff.toFixed(2)
+}
+
 export async function testGetContributorCount(githubToken: string) {
   console.log('Case: Many contributors')
   console.log('Expected:', 311)
-  console.log('Actual:', await getContributorCount('desktop', 'desktop', githubToken))
+  const actualManyContributors = await getContributorCount('desktop', 'desktop', githubToken)
+  console.log('Actual:', actualManyContributors)
+  console.log('Diff:', await calculatePercentageDifference(311, actualManyContributors))
 
   console.log('Case: Few contributors')
   console.log('Expected:', 20)
-  console.log('Actual:', await getContributorCount('microsoft', 'AI-For-Beginners', githubToken))
+  const actualFewContributors = await getContributorCount(
+    'microsoft',
+    'AI-For-Beginners',
+    githubToken
+  )
+  console.log('Actual:', actualFewContributors)
+  console.log('Diff:', await calculatePercentageDifference(20, actualFewContributors))
 
   console.log('Case: 7 contributors')
   console.log('Expected:', 7)
-  console.log('Actual:', await getContributorCount('Zeqiang-Lai', 'DragGAN', githubToken))
+  const actualSevenContributors = await getContributorCount('Zeqiang-Lai', 'DragGAN', githubToken)
+  console.log('Actual:', actualSevenContributors)
+  console.log('Diff:', await calculatePercentageDifference(7, actualSevenContributors))
 
   console.log('Case: 143 contributors')
   console.log('Expected:', 143)
-  console.log('Actual:', await getContributorCount('geohot', 'tinygrad', githubToken))
+  const actualOneFortyThreeContributors = await getContributorCount(
+    'geohot',
+    'tinygrad',
+    githubToken
+  )
+  console.log('Actual:', actualOneFortyThreeContributors)
+  console.log('Diff:', await calculatePercentageDifference(143, actualOneFortyThreeContributors))
 
   console.log('Case: 1623 contributors')
   console.log('Expected:', 1623)
-  console.log('Actual:', await getContributorCount('facebook', 'react', githubToken))
+  const actualOneSixTwoThreeContributors = await getContributorCount(
+    'facebook',
+    'react',
+    githubToken
+  )
+  console.log('Actual:', actualOneSixTwoThreeContributors)
+  console.log('Diff:', await calculatePercentageDifference(1623, actualOneSixTwoThreeContributors))
 
   console.log('Case: 63 contributors')
   console.log('Expected:', 63)
-  console.log('Actual:', await getContributorCount('spacedriveapp', 'spacedrive', githubToken))
+  const actualSixtyThreeContributors = await getContributorCount(
+    'spacedriveapp',
+    'spacedrive',
+    githubToken
+  )
+  console.log('Actual:', actualSixtyThreeContributors)
+  console.log('Diff:', await calculatePercentageDifference(63, actualSixtyThreeContributors))
 
   console.log('Case: 22 contributors')
   console.log('Expected:', 22)
-  console.log('Actual:', await getContributorCount('iam-abbas', 'Reddit-Stock-Trends', githubToken))
+  const actualTwentyTwoContributors = await getContributorCount(
+    'iam-abbas',
+    'Reddit-Stock-Trends',
+    githubToken
+  )
+  console.log('Actual:', actualTwentyTwoContributors)
+  console.log('Diff:', await calculatePercentageDifference(22, actualTwentyTwoContributors))
 
   console.log('Case: 75 contributors')
   console.log('Expected:', 75)
-  console.log('Actual:', await getContributorCount('GeneralMills', 'pytrends', githubToken))
+  const actualSeventyFiveContributors = await getContributorCount(
+    'GeneralMills',
+    'pytrends',
+    githubToken
+  )
+  console.log('Actual:', actualSeventyFiveContributors)
+  console.log('Diff:', await calculatePercentageDifference(75, actualSeventyFiveContributors))
 
   console.log('Case: 3 contributors')
   console.log('Expected:', 3)
-  console.log(
-    'Actual:',
-    await getContributorCount('vitalets', 'github-trending-repos', githubToken)
+  const actualThreeContributors = await getContributorCount(
+    'vitalets',
+    'github-trending-repos',
+    githubToken
   )
+  console.log('Actual:', actualThreeContributors)
+  console.log('Diff:', await calculatePercentageDifference(3, actualThreeContributors))
+
   console.log('Case: 0 contributors')
   console.log('Expected:', 0)
-  console.log(
-    'Actual:',
-    await getContributorCount('mbadry1', 'Trending-Deep-Learning', githubToken)
+  const actualZeroContributors = await getContributorCount(
+    'mbadry1',
+    'Trending-Deep-Learning',
+    githubToken
   )
+  console.log('Actual:', actualZeroContributors)
+  console.log('Diff:', await calculatePercentageDifference(0, actualZeroContributors))
 
   console.log('Case: 17 contributors')
   console.log('Expected:', 17)
-  console.log('Actual:', await getContributorCount('QasimWani', 'LeetHub', githubToken))
+  const actualSeventeenContributors = await getContributorCount('QasimWani', 'LeetHub', githubToken)
+  console.log('Actual:', actualSeventeenContributors)
+  console.log('Diff:', await calculatePercentageDifference(17, actualSeventeenContributors))
 
   console.log('Case: 0 contributors')
   console.log('Expected:', 0)
-  console.log('Actual:', await getContributorCount('datawrangling', 'trendingtopics', githubToken))
+  const actualZeroContributors2 = await getContributorCount(
+    'datawrangling',
+    'trendingtopics',
+    githubToken
+  )
+  console.log('Actual:', actualZeroContributors2)
+  console.log('Diff:', await calculatePercentageDifference(0, actualZeroContributors2))
+
+  console.log('Case: Many contributors')
+  console.log('Expected:', 4725)
+  const actualManyContributors2 = await getContributorCount(
+    'freeCodeCamp',
+    'freeCodeCamp',
+    githubToken
+  )
+  console.log('Actual:', actualManyContributors2)
+  console.log('Diff:', await calculatePercentageDifference(4725, actualManyContributors2))
+
+  console.log('Case: Few contributors')
+  console.log('Expected:', 2515)
+  const actualFewContributors2 = await getContributorCount(
+    'EbookFoundation',
+    'free-programming-books',
+    githubToken
+  )
+  console.log('Actual:', actualFewContributors2)
+  console.log('Diff:', await calculatePercentageDifference(2515, actualFewContributors2))
+
+  console.log('Case: 290 contributors')
+  console.log('Expected:', 290)
+  const actualTwoNineZeroContributors = await getContributorCount(
+    'jwasham',
+    'coding-interview-university',
+    githubToken
+  )
+  console.log('Actual:', actualTwoNineZeroContributors)
+  console.log('Diff:', await calculatePercentageDifference(290, actualTwoNineZeroContributors))
+
+  console.log('Case: 565 contributors')
+  console.log('Expected:', 565)
+  const actualFiveSixFiveContributors = await getContributorCount(
+    'sindresorhus',
+    'awesome',
+    githubToken
+  )
+  console.log('Actual:', actualFiveSixFiveContributors)
+  console.log('Diff:', await calculatePercentageDifference(565, actualFiveSixFiveContributors))
+
+  console.log('Case: 630 contributors')
+  console.log('Expected:', 620)
+  const actualSixThreeZeroContributors = await getContributorCount(
+    'kamranahmedse',
+    'developer-roadmap',
+    githubToken
+  )
+  console.log('Actual:', actualSixThreeZeroContributors)
+  console.log('Diff:', await calculatePercentageDifference(620, actualSixThreeZeroContributors))
 }
 
-void testGetContributorCount('token')
+void testGetContributorCount('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')
